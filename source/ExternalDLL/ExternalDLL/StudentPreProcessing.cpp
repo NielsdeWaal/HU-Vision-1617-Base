ca392e9ac741245ce1911bf6e7509feac916029b
philippezwietering <philippe.zwietering@gmail.com>
2017-04-04 14:09:13 +0200

    Added more bicubic algorithm stuff

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -89,40 +89,47 @@ static IntensityImage *scaleNearestNeighborMt(const IntensityImage &image,
     return out;
 }
 
-static IntensityImage *scaleBicubic(const IntensityImage &image){
-  auto dim = getNewDimensions(image);
-  const double origScale = 1 / std::get<2>(dim);
+constexpr double cubicInterpolate(const double p[4], const double x){
+    return p[1] + 0.5 * x * (p[2] - p[0] + x * (2.0 * p[0] - 5.0 * p[1] + 4.0 * p[2] - p[3] + x * (3.0 * (p[1] - p[2]) + p[3] - p[0])));
+}
 
-  IntensityImage *out = ImageFactory::newIntensityImage(std::get<0>(dim), std::get<1>(dim));
+double bicubicInterpolate(const double p[4][4], const double x, const double y){
+    double h[4];
+    for(uint8_t i = 0; i < 4; ++i){
+        h[i] = cubicInterpolate(p[i], y);
+    }
+    return cubicInterpolate(h, x);
+}
 
-  auto cores = std::thread::hardware_concurrency();
-  if(!cores){
-    cores = 2;
-  }
-  std::cerr << "got " << cores << " threads\n";
+static IntensityImage *scaleBicubic(const IntensityImage &image,
+                                              std::tuple<unsigned,unsigned,double> dim,
+                                              IntensityImage *out) {
 
-  size_t rowsPerThread = std::get<1>(dim) / cores;
+    const double origScale = 1 / std::get<2>(dim);
 
-  std::vector<std::thread> pool;
 
-  for(size_t i = 0; i < cores; ++i){
-    pool.emplace_back([&image, &dim, origScale, out](size_t rowStart, size_t rowCount){
-        for(uint y = rowStart + 2; y < rowStart + rowCount - 2; ++y){
+    draadificeer(std::get<1>(dim), [&image, &dim, origScale, out](size_t rowStart, size_t rowCount) {
+            for (uint y = rowStart; y < rowStart + rowCount; ++y) {
 
-          uint origY = round(origScale * y);
-          for(uint x = 2; x < std::get<0>(dim) - 2; ++x){
-            uint origX = round(origScale * x);
-            out->setPixel()
-          }
-        }
-      }, i * rowsPerThread,rowsPerThread + (i == cores-1 ? std::get<1>(dim) % rowsPerThread : 0));
+                double origY = origScale * y;
 
-  }
-  for(auto &t : pool)
-    t.join():
+                for (uint x = 0; x < std::get<0>(dim); ++x) {
+                    double origX = origScale * x;
 
-  return out;
+                    double p[4][4];
 
+                    for(int row = 0; row < 4; ++row){
+                        for(int col = 0; col < 4; ++col){
+                            p[col][row] = image.getPixel((unsigned)origX - 1 + col, (unsigned)origY - 1 + row);
+                        }
+                    }
+
+                    out->setPixel(x, y, bicubicInterpolate(p, origX-floor(origX), origY-floor(origY)));
+                }
+            }
+        });
+
+    return out;
 }
 
 #include <chrono>
@@ -149,8 +156,13 @@ IntensityImage *StudentPreProcessing::stepScaleImage(const IntensityImage &image
         auto dim = getNewDimensions(image);
         IntensityImage *out = ImageFactory::newIntensityImage(std::get<0>(dim), std::get<1>(dim));
 
+        // Intensity testpuntjes[4][4] = {{0, 30, 50, 20}, {10, 40, 70, 20}, {10, 2, 50, 30}, {0, 20, 80, 30}};
+        // std::cout << "Testbicubic: " << bicubicInterpolate(testpuntjes, 0.1, 0.1) << std::endl;
+        // std::cout << "Testcubic: " << cubicInterpolate(testpuntjes[0], 0.1) << std::endl;
+
+
         if (image.getWidth() * image.getHeight() > 40000)
-            ret = scaleNearestNeighborMt(image, dim, out);
+            ret = scaleBicubic(image, dim, out);
         else
             ret = ImageFactory::newIntensityImage(image);
     } else {
