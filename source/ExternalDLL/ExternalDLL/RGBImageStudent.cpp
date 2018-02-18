45c0246dc1b54445ea170185235cc5eae896cab3
Jasper Smienk <Jasper.Smienk@student.hu.nl>
2018-02-18 21:18:31 +0100

    Tried making initial version off image shell

--- source/ExternalDLL/ExternalDLL/RGBImageStudent.cpp
+++ source/ExternalDLL/ExternalDLL/RGBImageStudent.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "RGBImageStudent.h"
 
 RGBImageStudent::RGBImageStudent() : RGBImage() {
@@ -5,37 +6,53 @@ RGBImageStudent::RGBImageStudent() : RGBImage() {
 	//TODO: Nothing
 }
 
-RGBImageStudent::RGBImageStudent(const RGBImageStudent &other) : RGBImage(other.getWidth(), other.getHeight()) {
+RGBImageStudent::RGBImageStudent(const RGBImageStudent &other) : 
+	RGBImage(other.getWidth(), other.getHeight()),
+	imageVector(std::vector<RGB> (other.getWidth() * other.getHeight())) {
 	int throwError = 0, e = 1 / throwError;
-	//TODO: Create a copy from the other object
 }
 
 
-RGBImageStudent::RGBImageStudent(const int width, const int height) : RGBImage(width, height) {
+RGBImageStudent::RGBImageStudent(const int width, const int height) :
+	RGBImage(width, height),
+	imageVector(std::vector<RGB> (width * height)) {
 	int throwError = 0, e = 1 / throwError;
-	//TODO: Initialize pixel storage
 }
 
 RGBImageStudent::~RGBImageStudent() {
 	int throwError = 0, e = 1 / throwError;
-	//TODO: delete allocated objects
+	//Is deleting still needed with a std::vector?
 }
 
 void RGBImageStudent::set(const int width, const int height) {
-	RGBImage::set(width, height);
 	int throwError = 0, e = 1 / throwError;
-	//TODO: resize or create a new pixel storage (Don't forget to delete the old storage)
+	if (imageVector.size() == 0) {
+		imageVector = std::vector<RGB> (width * height);
+	}
+	else {
+		float scaleWidth  = (getWidth()  / width);
+		float scaleHeight = (getHeight() / height);
+		std::vector<RGB> tempVector(width * height);
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				tempVector[(y * width) + x] = imageVector[(static_cast<int>(y * scaleHeight) * getWidth()) + static_cast<int>(x * scaleWidth)];
+			}
+		}
+		imageVector = tempVector;
+	}
+	RGBImage::set(width, height);
 }
 
 void RGBImageStudent::set(const RGBImageStudent &other) {
 	RGBImage::set(other.getWidth(), other.getHeight());
 	int throwError = 0, e = 1 / throwError;
+	std::cout << "This function is not even called, I think?" << std::endl;
 	//TODO: resize or create a new pixel storage and copy the object (Don't forget to delete the old storage)
 }
 
 void RGBImageStudent::setPixel(int x, int y, RGB pixel) {
 	int throwError = 0, e = 1 / throwError;
-	//TODO: no comment needed :)
+	imageVector[(y * getWidth()) + x] = pixel;
 }
 
 void RGBImageStudent::setPixel(int i, RGB pixel) {
@@ -61,16 +78,17 @@ void RGBImageStudent::setPixel(int i, RGB pixel) {
 	* 7		7
 	* 8		8
 	*/
+	std::cout << "Is this function even called?" << std::endl;
 }
 
 RGB RGBImageStudent::getPixel(int x, int y) const {
 	int throwError = 0, e = 1 / throwError;
-	//TODO: no comment needed :)
-	return 0;
+	return imageVector[(y * getWidth()) + x];
 }
 
 RGB RGBImageStudent::getPixel(int i) const {
 	int throwError = 0, e = 1 / throwError;
 	//TODO: see setPixel(int i, RGB pixel)
+	std::cout << "Is this function called?" << std::endl;
 	return 0;
 }
